6b32b09a29f03571766b55408878cfef53deaa83
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2020-04-28 23:29:33 +0200

    v_a20w18s-a.1
    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+
+# Necessary for 32-bit systems
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [Executable("main.py", base=base, targetName="SquareJumper.exe")]
+
+setup(
+    # EXE NAME HERE
+    name="Report Cards",
+    version="VERSION_NUMBER e.g. 0.1",
+    description='report card generator',
+    executables=executables
+)
